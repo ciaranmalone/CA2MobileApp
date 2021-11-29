348eb81f9a1511467875bd79019ac5c06489711c
ciaranmalone <ciaranmalone27@gmail.com>
2021-11-29 21:48:53 +0000

    menus done

--- app/src/main/java/ciaran/malone/ca2mobileapp/activities/Game.kt
+++ app/src/main/java/ciaran/malone/ca2mobileapp/activities/Game.kt
@@ -1,13 +1,12 @@
-package ciaran.malone.ca2mobileapp
+package ciaran.malone.ca2mobileapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ciaran.malone.ca2mobileapp.R
 
-class MainActivity : AppCompatActivity() {
+class Game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-`
-
+        setContentView(R.layout.activity_game)
     }
 }
---
+++ app/src/main/java/ciaran/malone/ca2mobileapp/activities/HomeActivity.kt
@@ -0,0 +1,25 @@
+package ciaran.malone.ca2mobileapp.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ciaran.malone.ca2mobileapp.R
+import ciaran.malone.ca2mobileapp.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setTitle("Sonic fan app")
+
+        binding.imageButton.setOnClickListener {
+            val intent = Intent(this, Game::class.java)
+            startActivity(intent)
+        }
+    }
+}
